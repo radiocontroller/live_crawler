b7c680153dc62b97ec8648cf8d370ab1bcfc0be1
radiocontroller <302643159@qq.com>
2018-05-16 17:05:32 +0800

    优化

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,29 +1,5 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
-# crontab定时器最小单位为分钟，若改秒进入配置文件中改
-env :PATH, ENV['PATH']
-env :GEM_PATH, "/usr/local/bundle/"
 set :output, '/crawler_with_sinatra/log/cron.log'
 set :bundle_command, "/usr/local/bundle/bin/bundle exec"
-every 1.minute do
+every 2.minutes do
   rake "crawler:crawl_all"
 end
