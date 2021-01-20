1712429f755fff483d191257f304bddb38f7a95a
rcer <302643159@qq.com>
2021-01-20 16:09:00 +0800

    request.cookies[admin]

--- app.rb
+++ app.rb
@@ -37,25 +37,9 @@ class App < Sinatra::Base
   configure :production do
     enable :logging
 
-    get '/500w' do
-      draw = proc do
-        arr = (1..33).to_a.shuffle
-        r1 = 6.times.map { |idx| '%02d' % arr[idx] }
-
-        arr = (1..16).to_a.shuffle
-        r2 = Array('%02d' % arr[0])
-
-        (r1 + r2)
-      end
-      @numbers = []
-      num = (params[:num] || 5).to_i
-      num = 5 if num > 100
-      num.times.each { |_| @numbers << draw.call }
-      erb :draw, layout: false
-    end
-
     KINDS.each do |kind, cache_key|
       get "/#{kind}" do
+        @admin = request.cookies['admin']
         @lives = Redis.current.zrevrange(cache_key, 0, -1, with_scores: true)
                        .paginate(page: params[:page] || 1, per_page: PAGE_SIZE)
         erb :index, layout: :'layout'
@@ -64,6 +48,7 @@ class App < Sinatra::Base
       next if kind != 'lol'
 
       get '/' do
+        @admin = request.cookies['admin']
         @lives = Redis.current.zrevrange(cache_key, 0, -1, with_scores: true)
                        .paginate(page: params[:page] || 1, per_page: PAGE_SIZE)
         erb :index, layout: :'layout'
